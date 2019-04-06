c1bd5c49adb9b57aa949f464b79016f961f4f73e
Esteban Borai <estebanborai@outlook.com>
2019-04-05 21:35:41 -0300

    Add 3.18

---
+++ htp/exercises/ch03/3.18/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+double comission(double grossSales) {
+  return grossSales * 0.09;
+}
+
+int main() {
+  const int baseSalary = 200;
+  double grossSales;
+
+  while (grossSales > -1) {
+    printf("Enter sales in dollars (-1 to end): ");
+    scanf("%lf", &grossSales);
+
+    if (grossSales <= -1) {
+      break;
+    }
+
+    printf("Salary is $%.2f\n", (comission(grossSales) + baseSalary));
+  }
+}
