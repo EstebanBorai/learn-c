50431777fb062fd3aca58363abedcf665b548dc0
Esteban Borai <estebanborai@outlook.com>
2019-04-09 21:38:46 -0300

    Fix palindrome

--- htp/exercises/ch03/3.34/main.c
+++ htp/exercises/ch03/3.34/main.c
@@ -1,53 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-int promptInteger(char* message) {
-  int input;
-
-  printf("%s", message);
-  scanf("%d", &input);
-
-  return input;
-}
-
-int intLength(int number) {
-  return log10(number) + 1;
-}
-
-int* intToArray(int number) {
-  // Gets size of number
-  int size = intLength(number);
-
-  // Creates an char array pointer of the size
-  // of "size"
-  int* arr = calloc(size, sizeof(int));
-
-  // save each digit inside the array
-  for (int i = 0; i < size; i++) {
-    arr[i] = number % 10;
-    number /= 10;
-  }
-
-  // return the array
-  return arr;
-}
 
 int main() {
-  int a, equals = 0;
+  int num, reversed = 0, remainder, original;
 
-  a = promptInteger("Enter a number: ");
-  int numberSize = intLength(a);
+  printf("Enter an integer: ");
+  scanf("%d", &num);
 
-  int* base[5];
-  strcpy(base, intToArray(a));
+  original = num;
 
-  for (int i = 0; i < numberSize; i++) {
-    if (base[i] == base[numberSize - i]) {
-      continue;
-    } else {
-      printf("%d is not palindrome\n", a);
-      break;
-    }
+  while (num != 0) {
+    remainder = num % 10;
+    reversed = reversed * 10 + remainder;
+    num /= 10;
+  }
+
+  if (original == reversed) {
+    printf("%d is a palindrome.\n", original);
+  } else {
+    printf("%d is not a palindrome.\n", original);
   }
 }
